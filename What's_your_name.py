7e7fe82dcbe57a2e787c81e6dc9da4f532eb225a
vijay <128715498+vijaykumar-143@users.noreply.github.com>
2024-05-17 22:13:25 +0530

    Create What's_your_name.py
    You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
    
    Hello firstname lastname! You just delved into python.
    
    Function Description
    
    Complete the print_full_name function in the editor below.
    
    print_full_name has the following parameters:
    
    string first: the first name
    string last: the last name
    Prints
    
    string: 'Hello  ! You just delved into python' where  and  are replaced with  and .
    Input Format
    
    The first line contains the first name, and the second line contains the last name.
    
    Constraints
    
    The length of the first and last names are each ≤ .
    
    Sample Input 0
    
    Ross
    Taylor
    Sample Output 0
    
    Hello Ross Taylor! You just delved into python.
    Explanation 0
    
    The input read by the program is stored as a string data type. A string is a collection of characters

---
+++ What's_your_name.py
@@ -0,0 +1,8 @@
+def print_full_name(first, last):
+   print("Hello " + first + " " + last + "! You just delved into python.")
+    # Write your code here
+
+if name == 'main':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
