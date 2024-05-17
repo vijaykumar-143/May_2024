6dfc4dc126331b823bc49183a56ae8d998652ad1
vijay <128715498+vijaykumar-143@users.noreply.github.com>
2024-05-17 22:08:05 +0530

    Create Cuboid.py
    volume of cuboid

---
+++ Cuboid.py
@@ -0,0 +1,6 @@
+#volume of cuboid
+length=int(input("enter the length of cuboid :"))
+width=int(input("enter the width of cuboid :"))
+height=int(input("enter the height of the cuboid :"))
+volume=length*width*height
+print("volume of cuboid is :",volume)
